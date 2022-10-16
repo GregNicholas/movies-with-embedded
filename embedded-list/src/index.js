17892af6f3d22d63ac9852b83b04c7f9844f0473
Greg Schoenberg <gregschoenberg@gmail.com>
2022-10-15 20:23:26 -0700

    cleanup development messages

--- embedded-list/src/index.js
+++ embedded-list/src/index.js
@@ -1,11 +1,5 @@
 const list = document.querySelector(".list");
 
-list.addEventListener("click", () => {
-  const newLi = document.createElement("li");
-  newLi.innerText = "New movmov";
-  list.appendChild(newLi);
-});
-
 window.addEventListener("message", (e) => {
   if (!e.origin.startsWith("https://movies-list-gregschoenberg.netlify.app")) {
     return;
